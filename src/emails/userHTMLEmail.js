ab73aada06edb8c61c5392e1c0279492831d0f40
Gustavo <gusta1296@gmail.com>
2022-10-21 17:05:08 -0700

    Fixed user and admin email

---
+++ src/emails/userHTMLEmail.js
@@ -0,0 +1,155 @@
+
+const userHTMLEmail = (userName) => {
+	return `<!DOCTYPE html>
+			<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
+			
+			<head>
+				<title></title>
+				<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+				<!--[if !mso]><!-->
+				<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+				<!--<![endif]-->
+				<style>
+					* {
+						box-sizing: border-box;
+					}
+			
+					body {
+						margin: 0;
+						padding: 0;
+					}
+			
+					a[x-apple-data-detectors] {
+						color: inherit !important;
+						text-decoration: inherit !important;
+					}
+			
+					#MessageViewBody a {
+						color: inherit;
+						text-decoration: none;
+					}
+			
+					p {
+						line-height: inherit
+					}
+			
+					.desktop_hide,
+					.desktop_hide table {
+						mso-hide: all;
+						display: none;
+						max-height: 0px;
+						overflow: hidden;
+					}
+			
+					@media (max-width:620px) {
+						.desktop_hide table.icons-inner {
+							display: inline-block !important;
+						}
+			
+						.icons-inner {
+							text-align: center;
+						}
+			
+						.icons-inner td {
+							margin: 0 auto;
+						}
+			
+						.row-content {
+							width: 100% !important;
+						}
+			
+						.mobile_hide {
+							display: none;
+						}
+			
+						.stack .column {
+							width: 100%;
+							display: block;
+						}
+			
+						.mobile_hide {
+							min-height: 0;
+							max-height: 0;
+							max-width: 0;
+							overflow: hidden;
+							font-size: 0px;
+						}
+			
+						.desktop_hide,
+						.desktop_hide table {
+							display: table !important;
+							max-height: none !important;
+						}
+					}
+				</style>
+			</head>
+			
+			<body style="background-color: transparent; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+				<table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: transparent;">
+					<tbody>
+						<tr>
+							<td>
+								<table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+									<tbody>
+										<tr>
+											<td>
+												<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 600px;" width="600">
+													<tbody>
+														<tr>
+															<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+																<table class="paragraph_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+																	<tr>
+																		<td class="pad">
+																			<div style="color:#101112;direction:ltr;font-family:'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;font-size:16px;font-weight:400;letter-spacing:0px;line-height:120%;text-align:left;mso-line-height-alt:19.2px;">
+																				<p style="margin: 0; margin-bottom: 16px;">Hey, it's Developer Gus...</p>
+																				<p style="margin: 0; margin-bottom: 16px;">&nbsp;</p>
+																				<p style="margin: 0; margin-bottom: 16px;">Thank you ${userName}, for reaching out to me with your message. I will make sure to get back to you shortly.&nbsp;</p>
+																				<p style="margin: 0; margin-bottom: 16px;">&nbsp;</p>
+																				<p style="margin: 0; margin-bottom: 16px;"><strong>- Developer Gus</strong></p>
+																				<p style="margin: 0;"><strong>P.S. Make sure to visit my YouTube channel and learn more about many coding topics including web development.&nbsp;</strong></p>
+																			</div>
+																		</td>
+																	</tr>
+																</table>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+								<table class="row row-2" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+									<tbody>
+										<tr>
+											<td>
+												<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 600px;" width="600">
+													<tbody>
+														<tr>
+															<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+																<table class="image_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+																	<tr>
+																		<td class="pad" style="width:100%;padding-right:0px;padding-left:0px;">
+																			<div class="alignment" align="left" style="line-height:10px"><a href="https://www.youtube.com/channel/UChuhEhCujTGP1mfmPdtuVhA" target="_blank" style="outline:none" tabindex="-1"><img src="https://portfolio-backend-94.herokuapp.com/images/social-media/youtube-channel-of-developer-gus-2.png" style="display: block; height: auto; border: 0; width: 120px; max-width: 100%;" width="120" alt="YouTube Channel of Developer Gus" title="YouTube Channel of Developer Gus"></a></div>
+																		</td>
+																	</tr>
+																</table>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+						</tr>
+					</tbody>
+				</table><!-- End -->
+			</body>
+			</html>`;
+}
+
+module.exports = userHTMLEmail;
